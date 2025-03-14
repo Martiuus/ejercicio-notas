eb361d4381c787c02733700a6a2e6e3c33f5a824
aaronp636 <aaronp636@gmail.com>
2025-03-13 21:35:21 -0500

    Primer commit Java

--- src/main/java/pe/edu/idat/ejercicio_notas/controller/promedioController.java
+++ src/main/java/pe/edu/idat/ejercicio_notas/controller/promedioController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class promedioController {
 
-    @GetMapping("/notas")
+    @GetMapping("/promedio")
     public String formularioNotas(){
-        return "notas";
+        return "promedio";
     }
 }
