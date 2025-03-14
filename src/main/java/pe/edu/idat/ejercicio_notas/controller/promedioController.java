bcf91c3e74b9576c574cf56c10657977d2b6b16e
Martiuus <mzf.ycb.11@gmail.com>
2025-03-13 21:12:02 -0500

    Empezamos soli

---
+++ src/main/java/pe/edu/idat/ejercicio_notas/controller/promedioController.java
@@ -0,0 +1,4 @@
+package pe.edu.idat.ejercicio_notas.controller;
+
+public class promedioController {
+}
---
+++ src/main/java/pe/edu/idat/ejercicio_notas/model/promedioModel.java
@@ -0,0 +1,4 @@
+package pe.edu.idat.ejercicio_notas.model;
+
+public class promedioModel {
+}
