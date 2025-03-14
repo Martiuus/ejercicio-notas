97ad05d6f9ffca9d6be4a191fe95bb925e8ee449
aaronp636 <aaronp636@gmail.com>
2025-03-13 22:28:26 -0500

    Falta color

--- src/main/java/pe/edu/idat/ejercicio_notas/controller/promedioController.java
+++ src/main/java/pe/edu/idat/ejercicio_notas/controller/promedioController.java
@@ -13,17 +13,29 @@ public class promedioController {
     @GetMapping("/promedio")
     public String formularioNotas(Model model){
         model.addAttribute("promediomodel", new promedioModel());
+        model.addAttribute("visualizaralerta", false);
         return "promedio";
     }
 
+    @PostMapping("/calcularpromedio")
     public String calcularPromedio(@ModelAttribute("promediomodel") promedioModel pm , Model model){
         Double nota1m = pm.getNota1();
         Double nota2m = pm.getNota2();
         Double nota3m = pm.getNota3();
         Double nota4m = pm.getNotaFinal();
+        String condicion = "";
+        String ColorResultado = "alert-primary";
 
         Double resultado = (nota1m * 0.04) + (nota2m * 0.12) + (nota3m * 0.24) + (nota4m * 0.6);
-
+        if (resultado < 13){
+            condicion = "Desprobado";
+            ColorResultado = "alert-danger";
+        }else {
+            condicion = "Aprobado";
+        }
+        model.addAttribute("resultado", condicion + ", nota: " + resultado);
+        model.addAttribute("visualizaralerta", true);
+        model.addAttribute("color", ColorResultado);
         return "promedio";
 
     }
