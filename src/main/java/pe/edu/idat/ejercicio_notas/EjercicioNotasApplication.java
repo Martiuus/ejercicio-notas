fb0d9e4a8874ef7574f6004a15ea09dec58c6ec7
Martiuus <mzf.ycb.11@gmail.com>
2025-03-13 21:09:44 -0500

    Estructura del ejercicio

---
+++ src/main/java/pe/edu/idat/ejercicio_notas/EjercicioNotasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ejercicio_notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/ejercicio_notas/EjercicioNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ejercicio_notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
